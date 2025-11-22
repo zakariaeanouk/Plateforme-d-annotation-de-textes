31a31413f81a67a9f4a4af6656b6bceadc4778bf
zakariaeanouk <150734633+zakariaeanouk@users.noreply.github.com>
2025-11-22 02:04:23 +0100

    Remove extraneous characters from AnnotationPlatfromApplication

--- src/main/java/com/AnnotationPlatform/AnnotationPlatfromApplication.java
+++ src/main/java/com/AnnotationPlatform/AnnotationPlatfromApplication.java
@@ -9,5 +9,5 @@ public class AnnotationPlatfromApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnnotationPlatfromApplication.class, args);
     }
-
+#khgkjhgjh
 }
